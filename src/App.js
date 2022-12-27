96f8932900f23e4ce814516218425d00eb78da2f
Artessay <rihong.edu@outlook.com>
2022-12-27 08:03:02 +0800

    last night

--- src/App.js
+++ src/App.js
@@ -2,27 +2,140 @@ import './App.css';
 
 function App() {
   return (
-    <header>
-      <h1>World Happiness Report</h1>
-      <div class="showTime" onLoad={showTime()}></div>
-    </header>
-    // <h1>Hello World</h1>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+    <div className="App">
+      <header className="App-header">
+        <h1>Basketball Visual News</h1>
+        <div class="showTime" onLoad={showTime()}></div>
+      </header>
+      <body>
+        <div class="sidebar">
+          <br></br>
+          <h1>导航栏</h1>
+        </div>
+        <div>
+          
+        </div>
+        <div class="mainbox">
+          <div class="column">
+            <div class="panel world">
+              <h2>幸福指数世界地图</h2>
+              <div class="chart"></div>
+              <div class="panel-footer"></div>
+            </div>
+
+            <div class="panel pie">
+              <h2>幸福指数-GDP相关性图</h2>
+              <div class="chart"></div>
+              <div class="panel-footer"></div>
+            </div>
+
+            <div class="panel river">
+              <h2>各地区幸福指数分布图</h2>
+              <div class="chart"></div>
+              <div class="panel-footer"></div>
+            </div>
+          </div>
+
+          <div class="column">
+            <div class="panel bar">
+              <h2>幸福指数柱状图</h2>
+              <div class="chart"></div>
+              <div class="panel-footer"></div>
+            </div>
+
+            <div class="panel line">
+              <h2>幸福指数聚类分布曲线</h2>
+              <div class="chart"></div>
+              <div class="panel-footer"></div>
+            </div>
+
+            <div class="panel heatmap">
+              <h2>二元变量相关分析</h2>
+              <div class="chart"></div>
+              <div class="panel-footer"></div>
+            </div>
+          </div>
+        </div>
+
+        {/* <section class="box">
+          <div class="panel full">
+            <h2>2021年各地区幸福指数分布</h2>
+            <div class="chart"></div>
+            <div class="panel-footer"></div>
+          </div>
+        </section>
+
+        <section class="box">
+          <div class="panel full">
+            <h2>各影响因素的KMEANS聚类分析（K=3）</h2>
+            <div class="chart">
+              <div class="column">
+                <div class="subchart ladder_score"></div>
+              </div>
+              <div class="column">
+                <div class="subchart2 healthy_life_expectancy"></div>
+                <div class="subchart2 freedom_to_make_life_choices"></div>
+              </div>
+              <div class="column">
+                <div class="subchart2 generosity"></div>
+                <div class="subchart2 social_support"></div>
+              </div>
+              <div class="column">
+                <div class="subchart2 perceptions_of_corruption"></div>
+                <div class="subchart2 logged_gdp_per_capita"></div>
+              </div>
+            </div>
+            <div class="panel-footer"></div>
+          </div>
+        </section>
+
+        <section class="box">
+          <div class="panel full">
+            <h2>二元变量散点图</h2>
+            <div class="select">
+              <label for="xAxis">Choose xAxis:</label>
+              <select id="xAxis" name="xAxis">
+                <option value="0" selected>freedom</option>
+                <option value="1">generosity</option>
+                <option value="2">life expectancy</option>
+                <option value="3">ladder score</option>
+                <option value="4">gdp</option>
+                <option value="5">negative</option>
+                <option value="6">corruption</option>
+                <option value="7">positive</option>
+                <option value="8">social support</option>
+                <option value="9">year</option>
+              </select>
+
+              <label for="yAxis">Choose yAxis:</label>
+              <select id="yAxis" name="yAxis">
+                <option value="10">freedom</option>
+                <option value="11" selected>generosity</option>
+                <option value="12">life expectancy</option>
+                <option value="13">ladder score</option>
+                <option value="14">gdp</option>
+                <option value="15">negative</option>
+                <option value="16">corruption</option>
+                <option value="17">positive</option>
+                <option value="18">social support</option>
+                <option value="19">year</option>
+              </select>
+            </div>
+
+            <div class="chart dot"></div>
+            <div class="panel-footer"></div>
+          </div>
+        </section>
+
+        <section class="box">
+          <div class="panel hm">
+            <h2>二元变量相关性分析</h2>
+            <div class="chart"></div>
+            <div class="panel-footer"></div>
+          </div>
+        </section> */}
+      </body>
+    </div>
   );
 }
 
